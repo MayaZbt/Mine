8bb57c5768cb7af5400a10cc86787e5b510f45db
MayaZbt <64219167+MayaZbt@users.noreply.github.com>
2020-07-06 20:22:14 +0300

    Task1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 import "./styles.css";
 
+function MovieItem(props) {
+  console.log("props = ", props);
+  return (
+    <div>
+      <p>{props.title}</p>
+      <p>{props.vote_average}</p>
+    </div>
+  );
+}
+
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <MovieItem title="Title new" vote_average={10.0} />
     </div>
   );
 }
